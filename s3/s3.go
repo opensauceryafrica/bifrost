7ff2dc6f1dac8e3128128ac2e88910ff02298e10
Open Saucerer <lovedayperfection1@gmail.com>
2023-01-06 14:12:04 +0100

    feat: added support for options to S3 implementation and updated documentation

--- s3/s3.go
+++ s3/s3.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	awsTypes "github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/opensaucerer/bifrost/shared/config"
 	"github.com/opensaucerer/bifrost/shared/errors"
 	"github.com/opensaucerer/bifrost/shared/types"
 )
@@ -23,16 +24,15 @@ func (s *SimpleStorageService) UploadFile(path, filename string, options map[str
 	}
 	// verify that file exists
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return &types.UploadedFile{}, &errors.BifrostError{
+		return nil, &errors.BifrostError{
 			Err:       fmt.Errorf("file does not exist: %s", path),
 			ErrorCode: errors.ErrBadRequest,
 		}
 	}
-
 	// open file
 	file, err := os.Open(path)
 	if err != nil {
-		return &types.UploadedFile{}, &errors.BifrostError{
+		return nil, &errors.BifrostError{
 			Err:       err,
 			ErrorCode: errors.ErrFileOperationFailed,
 		}
@@ -40,31 +40,68 @@ func (s *SimpleStorageService) UploadFile(path, filename string, options map[str
 	// close file
 	defer file.Close()
 
-	// Upload the file to S3
-	if s.PublicRead {
-		_, err = s.Client.PutObject(ctx, &s3.PutObjectInput{
-			Bucket: aws.String(s.DefaultBucket),
-			Key:    aws.String(filename),
-			Body:   file,
-			ACL:    awsTypes.ObjectCannedACLPublicRead,
-		})
-	} else {
-		_, err = s.Client.PutObject(ctx, &s3.PutObjectInput{
-			Bucket: aws.String(s.DefaultBucket),
-			Key:    aws.String(filename),
-			Body:   file,
-		})
+	var params *s3.PutObjectInput = &s3.PutObjectInput{
+		Bucket: aws.String(s.DefaultBucket),
+		Key:    aws.String(filename),
+		Body:   file,
 	}
-	if err != nil {
-		return &types.UploadedFile{}, &errors.BifrostError{
+	// check the bridge config for default acl settings
+	if s.PublicRead {
+		// set public read permissions
+		params.ACL = awsTypes.ObjectCannedACLPublicRead
+	}
+	// set file permissions
+	for k, v := range options {
+		switch k {
+		// check the options map for acl settings
+		case config.OptACL:
+			if v, ok := v.(string); ok {
+				switch v {
+				case config.ACLPublicRead:
+					params.ACL = awsTypes.ObjectCannedACLPublicRead
+				case config.ACLPrivate:
+					params.ACL = awsTypes.ObjectCannedACLPrivate
+				}
+
+			}
+		// set the content type
+		case config.OptContentType:
+			if v, ok := v.(string); ok {
+				params.ContentType = aws.String(v)
+			}
+		// set object metadata
+		case config.OptMetadata:
+			if v, ok := v.(map[string]string); ok {
+				params.Metadata = v
+			}
+		}
+	}
+	// Upload the file to S3
+	if _, err := s.Client.PutObject(ctx, params); err != nil {
+		return nil, &errors.BifrostError{
 			Err:       err,
 			ErrorCode: errors.ErrFileOperationFailed,
 		}
 	}
+	// head object details
+	obj, err := s.Client.HeadObject(ctx, &s3.HeadObjectInput{
+		Bucket: aws.String(s.DefaultBucket),
+		Key:    aws.String(filename),
+	})
+	if err != nil {
+		return nil, &errors.BifrostError{
+			Err:       err,
+			ErrorCode: errors.ErrFileOperationFailed,
+		}
+	}
+	fmt.Println(obj.Metadata)
 	return &types.UploadedFile{
-		Name:   filename,
-		Bucket: s.DefaultBucket,
-		Path:   path,
+		Name:           filename,
+		Bucket:         s.DefaultBucket,
+		Path:           path,
+		Preview:        fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", s.DefaultBucket, s.Region, filename),
+		Size:           obj.ContentLength,
+		ProviderObject: obj,
 	}, nil
 }
 
@@ -83,8 +120,5 @@ func (s *SimpleStorageService) Config() *types.BridgeConfig {
 }
 
 func (s *SimpleStorageService) Disconnect() error {
-	if s.Client != nil {
-		// return s.Client.
-	}
 	return nil
 }
