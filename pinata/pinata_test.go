0a24fef80356b58f5e0985a16989882dc17a1bcd
Perfection Loveday <lovedayperfection1@gmail.com>
2023-01-15 08:33:36 +0100

    chore: updated test file

--- pinata/pinata_test.go
+++ pinata/pinata_test.go
@@ -7,15 +7,10 @@ import (
 	"testing"
 )
 
-type RainbowBridge interface {
-	UploadFile(path, filename string, options map[string]interface{}) (*types.UploadedFile, error)
-	Disconnect() error
-	Config() *types.BridgeConfig
-}
 
-func getRainboBridge() RainbowBridge {
+func TestUploadFileSucceeds(t *testing.T) {
 
-	PINATA_JWT := os.Getenv("PINATA_JWT")
+        PINATA_JWT := os.Getenv("PINATA_JWT")
 
 	bridge, err := bifrost.NewRainbowBridge(&bifrost.BridgeConfig{
 		PinataJWT: PINATA_JWT,
@@ -26,14 +21,8 @@ func getRainboBridge() RainbowBridge {
 		panic(err)
 	}
 
-	return bridge
-}
-func TestUploadFileSucceeds(t *testing.T) {
-
-	bridge := getRainboBridge()
-
-	options := map[string]any{"cidVersion": 1, "wrapWithDirectory": true}
-	_, err := bridge.UploadFile("./image/file.png","./image/file.png", options)
+	options := map[string]interface{"cidVersion": 1, "wrapWithDirectory": true}
+	_, err := bridge.UploadFile("./image/file.png", "./image/file.png", options)
 
 	if err != nil {
 		t.Errorf("Failed to upload file: %v", err)
