befbde12601432eb0a31716e48896379fea85d31
Open Saucerer <lovedayperfection1@gmail.com>
2023-04-03 05:36:06 +0100

    docs: updated package docs and spacing

--- shared/config/provider.go
+++ shared/config/provider.go
@@ -1,12 +1,14 @@
 package config
 
-// Misc constants
+// Provider constants
 const (
 
 	// PinataCloud is the identifier of the Pinata Cloud storage
 	PinataCloud = "pinata"
+
 	// SimpleStorageService is the identifier of the S3 provider
 	SimpleStorageService = "s3"
+
 	// GoogleCloudStorage is the identifier of the Google Cloud Storage provider
 	GoogleCloudStorage = "gcs"
 )
--- shared/config/url.go
+++ shared/config/url.go
@@ -1,19 +1,25 @@
 package config
 
-// Pinata URL constants.
+// URL constants.
 const (
 	// URLPinataPinFile is the endpoint for pinning files/folders to Pinata cloud.
 	URLPinataPinFile = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+
 	// URLPinataPinJSON is the endpoint for pinning JSON objects to Pinata cloud.
 	URLPinataPinJSON = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+
 	// URLPinataPinCID is the endpoint for pinning CIDs to Pinata cloud.
 	URLPinataPinCID = "https://api.pinata.cloud/pinning/pinByHash"
+
 	// URLPinataAuth is the endpoint for testing authentication against provided Pinata credentials
 	URLPinataAuth = "https://api.pinata.cloud/data/testAuthentication"
+
 	// URLPinataGateway is the public gateway for Pinata cloud.
 	URLPinataGateway = "https://gateway.pinata.cloud/ipfs/%v"
+
 	// URLGoogleCloudStorage is the public gateway for Google Cloud Storage.
 	URLGoogleCloudStorage = "https://storage.googleapis.com/%s/%s"
+
 	// URLSimpleStorageService is the public gateway for Simple Storage Service.
 	URLSimpleStorageService = "https://%s.s3.%s.amazonaws.com/%s"
 )
